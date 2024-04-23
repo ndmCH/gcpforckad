f153c0069acfefcd30b1acc30bbad871f4880553
nma <nils.marti@terreactive.ch>
2024-04-23 09:01:42 +0200

    locally applied config, this works

--- compute.tf
+++ compute.tf
@@ -1,13 +1,18 @@
 resource "google_compute_instance" "cp" {
   name         = "cp"
   machine_type = "e2-standard-2"
-  zone         = "europe-central6-a"
+  zone         = "europe-west6-a"
 
   boot_disk {
+    auto_delete = true
+
     initialize_params {
-      image = "ubuntu-2004-focal-v20230328"
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20240307b"
       size  = 20
+      type  = "pd-balanced"
     }
+
+    mode  = "READ_WRITE"
   }
 
   network_interface {
@@ -21,13 +26,18 @@ resource "google_compute_instance" "cp" {
 resource "google_compute_instance" "worker" {
   name         = "worker"
   machine_type = "e2-standard-2"
-  zone         = "europe-central6-a"
+  zone         = "europe-west6-a"
 
   boot_disk {
+    auto_delete = true
+
     initialize_params {
-      image = "ubuntu-2004-focal-v20230328"
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20240307b"
       size  = 20
+      type  = "pd-balanced"
     }
+
+    mode = "READ_WRITE"
   }
 
   network_interface {
